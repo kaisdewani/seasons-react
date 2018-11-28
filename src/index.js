7105e4959dee9be2bcd7b84b174cf43b27d6118c
Kais Dewani <kaisdewani@yahoo.com>
2018-11-28 15:12:59 -0500

    add helper method in App

--- src/index.js
+++ src/index.js
@@ -14,16 +14,25 @@ class App extends React.Component {
     );
   }
 
-  render () {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat){
       return <div>Error: { this.state.errorMessage }</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat){
-      return <SeasonDisplay lat= {this.state.lat} />
+      return <SeasonDisplay lat= {this.state.lat} />;
     }
 
     return <Spinner message="Please accept location request" />;
+  }
+
+
+  render () {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
